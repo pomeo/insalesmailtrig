6243be0959263e76c34eb9bdf03eb924b773d5fe
Sergey Ovechkin <me@sovechkin.com>
2014-08-04 08:46:37 +0400

    центральный журнал

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//Логгер в одном месте, для упрощения перезда на любой логгер.
+function log(logMsg) {
+  if (logMsg instanceof Error) console.log(logMsg.stack);
+  if (debugOn) {
+    if (typeof logMsg == 'object') {
+      console.dir(logMsg);
+    } else {
+      console.log(logMsg);
+    }
+  }
+};
