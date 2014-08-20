bb194fc6edcc3feb25a1c92c5c365daaca9c72fd
Sergey Ovechkin <me@sovechkin.com>
2014-08-20 16:31:59 +0400

    добавляем новые файлы на ходу в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,44 @@ var gulp = require('gulp'),
     reload = browserSync.reload;
 
 gulp.task('images', function () {
-  gulp.src('src/img/**/*')
-  .pipe(watch(function(files) {
-          return files.pipe(imagemin({
+  watch({glob: 'src/img/**/*'},
+        function(files) {
+          files.pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngcrush()]
           }))
-                 .pipe(gulp.dest('public/img'))
-                 .pipe(reload({stream:true}));
-        }));
+          .pipe(gulp.dest('public/img'))
+          .pipe(reload({stream:true}));
+        });
 });
 
 gulp.task('compress', function() {
-  gulp.src('src/js/**/*.js')
-  .pipe(watch(function(files) {
-          return files.pipe(uglify())
-                 .pipe(gulp.dest('public/js'))
-                 .pipe(reload({stream:true}));
-        }));
+  watch({glob: 'src/js/**/*.js'},
+        function(files) {
+          files.pipe(uglify())
+          .pipe(gulp.dest('public/js'))
+          .pipe(reload({stream:true}));
+        });
 });
 
 gulp.task('minify-css', function() {
-  gulp.src('src/css/**/*.css')
-  .pipe(watch(function(files) {
-          return files.pipe(minifyCSS())
-                 .pipe(gulp.dest('public/css'))
-                 .pipe(reload({stream:true}));
-        }));
+  watch({glob: 'src/css/**/*.css'},
+        function(files) {
+          files.pipe(minifyCSS())
+          .pipe(gulp.dest('public/css'))
+          .pipe(reload({stream:true}));
+        });
 });
 
 gulp.task('stylus', function () {
-  gulp.src('src/css/**/*.styl')
-  .pipe(watch(function(files) {
-          return files.pipe(stylus({compress: true, use: nib()}))
-                 .pipe(prefix())
-                 .pipe(gulp.dest('public/css'))
-                 .pipe(reload({stream:true}));
-        }));
+  watch({glob: 'src/css/**/*.styl'},
+        function(files) {
+          files.pipe(stylus({compress: true, use: nib()}))
+          .pipe(prefix())
+          .pipe(gulp.dest('public/css'))
+          .pipe(reload({stream:true}));
+        });
 });
 
 gulp.task('browser-sync', function() {
