e0fe01caf0d771ee8d8b0be10bceb948b6ab2b04
Sergey Ovechkin <me@sovechkin.com>
2014-09-20 13:31:35 +0400

    приводим в более нормальный json приходящий xml

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(xmlparser());
+app.use(xmlparser({trim: false, explicitArray: false, ignoreAttrs: true}));
 app.use(cookieParser());
 app.use(session({secret: process.env.SECRET, store: new MySQLStore(options)}))
 app.use(express.static(path.join(__dirname, 'public')));
